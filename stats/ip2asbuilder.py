d561800698ac9e6767469a64b05612726e2eaaf3
Christian Huitema <huitema@huitema.net>
2020-08-07 18:32:49 -0700

    Build more complete AS table

--- stats/ip2asbuilder.py
+++ stats/ip2asbuilder.py
@@ -70,10 +70,10 @@ nb_bad_line = 0
 nb_ipv4 = 0
 nb_ipv6 = 0
 
-ipdom = dict()
+initial_list = []
 
 try:
-    for line in open(ip2asn_file , "rt"):
+    for line in open(ip2asn_file , "rt", encoding='utf-8'):
         nb_line += 1
         strip_line = line.strip()
         parts = strip_line.split("\t")
@@ -87,14 +87,10 @@ try:
                 ip_dv4.ip_first = ipaddress.ip_address(parts[0])
                 ip_dv4.ip_last = ipaddress.ip_address(parts[1])
                 ip_dv4.as_number = int(parts[2])
-                if len(ipdom) < 10:
+                if len(initial_list) < 10:
                     print(str(ip_dv4.ip_first) + " ... " + str(ip_dv4.ip_last) + " --> " + str(ip_dv4.as_number))
-                if ip_dv4.ip_first in ipdom:
-                    if nb_duplicate < 10:
-                        print("duplicate: " + str(ip_dv4.ip_first) + " ... " + str(ip_dv4.ip_last))
-                    nb_duplicate += 1
-                else:
-                    ipdom[ip_dv4.ip_first] = ip_dv4          
+                initial_list.append(ip_dv4)
+                nb_ipv4 += 1        
             except Exception as e:
                 traceback.print_exc()
                 print("Got exception: " + str(e))
@@ -103,30 +99,76 @@ try:
                     print("Parse error: <" + line.strip() + ">")
     print("Found " + str(nb_line) + " lines in " + ip2asn_file )
 except:
+    traceback.print_exc()
+    print("Got exception: " + str(e))
     print("Error after " + str(nb_line) + " lines in " + ip2asn_file)
 
-print("Entries: " + str(len(ipdom)))
+print("Entries: " + str(len(initial_list)))
 print("Duplicates: " + str(nb_duplicates))
 print("Parse_error: " + str(nb_parse_error))
 print("Bad_line: " + str(nb_parse_error))
+print("IPv4: " + str(nb_ipv4))
 
-sorted_list = sorted(ipdom.values())
+sorted_list = sorted(initial_list)
+
+# display the beginning of the sorted list 
+
+nb_item = 0
+
+for item in sorted_list:
+    nb_item += 1
+    if nb_item < 20:
+         print(str(item.ip_first) + "..." + str(item.ip_last) + " : " + str(item.as_number))
+    else:
+        break
+
+# Create a compact list by reducing overlapping ranges
+
+compact_list = []
+heap = []
+
+for item in sorted_list:
+    if len(heap) == 0 or heap[0].ip_first == item.ip_first:
+        heap.append(item)
+    else:
+        # sort the heap so the shorted range is last
+        heap = sorted(heap)
+        # process the ranges in heap that precede the next item
+        while len(heap) > 0 and heap[len(heap) - 1].ip_last < item.ip_first:
+            heap_last = heap.pop()
+            append_item_to_compact_list(heap_last, compact_list, heap)
+        # manage partial overlap with last item
+        if len(heap) > 0 and heap[len(heap) - 1].ip_first < item.ip_first:
+            added_range = heap[len(heap) - 1]
+            added_range.ip_last = item.ip_first - 1       
+            append_item_to_compact_list(added_range, compact_list, heap)
+        # add current item to the heap
+        heap.append(item)
+
+# Add the reminder of the heap to the compacted ranges
+while len(heap) > 0:
+    heap_last = heap.pop()
+    append_item_to_compact_list(heap_last, compact_list, heap)
+
+print("Non overlapping ranges: " + str(len(compact_list)))
+
+# Display the beginning of the list
+
+nb_item = 0
+
+for item in compact_list:
+    nb_item += 1
+    if nb_item < 10:
+         print(str(item.ip_first) + "..." + str(item.ip_last) + " : " + str(item.as_number))
+    else:
+        break
 
 # save the result file
-
-overlap_found = False
 try:
     nb_saved = 0
-    ip_previous = ipaddress.ip_address("0.0.0.0")
-    after_first = False
     file_out = open(csv_file, "w")
     file_out.write("ip_first, ip_last, as_number,\n")
-    for item in sorted_list :
-        if after_first and item.ip_first <= ip_previous:
-            print("Found overlap, " + str(item.ip_first) + "," + str(ip_previous))
-            break
-        ip_previous = item.ip_first
-        after_first = True
+    for item in compact_list:
         file_out.write(str(item.ip_first) + "," + str(item.ip_last) + "," + str(item.as_number) + ",\n")
         nb_saved += 1
     file_out.close()
@@ -134,3 +176,4 @@ try:
 except Exception as e:
     traceback.print_exc()
     print("Cannot write <" + csv_file+ ">, error: " + str(e));
+
